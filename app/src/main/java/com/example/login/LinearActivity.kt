2ece1337464967f056e9c2665090fa8ed6cd87ac
Bruno Souza <brunoss2118@gmail.com>
2026-09-03 21:03:42 -0300

    first commit

---
+++ app/src/main/java/com/example/login/LinearActivity.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LinearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_demo)
+    }
+}
